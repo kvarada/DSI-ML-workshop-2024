fbd989665eb8138ec9697be59443a7aed4ee66b9
Varada Kolhatkar <kvarada@cs.ubc.ca>
2024-08-22 16:21:23 -0700

    adding PCA material

--- website/slides/code/unsupervised_learning_code.py
+++ website/slides/code/unsupervised_learning_code.py
@@ -136,4 +136,66 @@ def get_features(model, inputs):
     with torch.no_grad():  # turn off computational graph stuff        
         Z = model(inputs).detach().numpy()         
     return Z
+
+
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+def pca_faces(X_faces, n_components=[10, 50, 100, 200, 300]):
+    """
+    Applies PCA to a dataset of faces to reduce dimensionality and reconstructs the images.
+
+    Parameters
+    ----------
+    X_faces : np.ndarray
+        The input dataset of face images, where each row represents an image.
+    n_components : list of int, default=[10, 50, 100, 200, 300]
+        The number of principal components to retain for the reconstruction.
+
+    Returns
+    -------
+    list of np.ndarray
+        A list of numpy arrays, each representing the dataset reconstructed
+        from a different number of principal components.
+    """
+    reduced_images = []
+    for n in n_components:
+        pca = PCA(n_components=n)
+        pca.fit(X_faces)
+        X_hat = pca.inverse_transform(pca.transform(X_faces))
+        reduced_images.append(X_hat)
+    return reduced_images        
+
+def plot_pca_animals(X_faces, image_shape, n_components=[10, 50, 100, 200, 300], index=30):
+    """
+    Plots the original and PCA-reconstructed images from a dataset of animal faces.
+
+    Parameters
+    ----------
+    X_faces : np.ndarray
+        The input dataset of face images.
+    image_shape : tuple
+        The shape of a single image (height, width).
+    n_components : list of int, default=[10, 50, 100, 200, 300]
+        The number of principal components used for the reconstructions.
+    index : int, default=30
+        The starting index from which to plot the original and reconstructed images.
+
+    Returns
+    -------
+    None
+    """
+    reduced_images = pca_faces(X_faces, n_components)
+
+    fig, axes = plt.subplots(3, len(n_components) + 1, figsize=(15, 8),
+                             subplot_kw={'xticks': [], 'yticks': []})
+    for i, ax_row in enumerate(axes):
+        ax_row[0].imshow(X_faces[i + index].reshape(image_shape), vmin=0, vmax=1)
+        for ax, X_hat in zip(ax_row[1:], reduced_images):
+            ax.imshow(X_hat[i + index].reshape(image_shape), vmin=0, vmax=1)
+
+    axes[0, 0].set_title("Original image")
+    for ax, n in zip(axes[0, 1:], n_components):
+        ax.set_title(f"{n} components")
+    plt.show()
     
